5de584032767b3badf86bc569ba35dec5ab7c086
Tinu205 <dazleroger404@gmail.com>
2024-01-13 21:46:27 +0530

    Nav bar working

--- libs/load.php
+++ libs/load.php
@@ -1,7 +1,6 @@
 <?php
     function load_template($name){
-
-        include $_SERVER['DOCUMENT_ROOT']."/I3E_portal/libs/templates/$name.php";
-
+        include $_SERVER['DOCUMENT_ROOT']."/I3E_portal/templates/$name.php";
+        // print($_SERVER['DOCUMENT_ROOT']."/I3E_portal/libs/templates/$name.php");
     }
 ?>
