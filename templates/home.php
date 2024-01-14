dff1b63d61a7e18ea974a5b63f3f30d32bdfe7c8
Tinu205 <dazleroger404@gmail.com>
2024-01-14 23:00:32 +0530

    Fixed home page

--- templates/home.php
+++ templates/home.php
@@ -1,16 +1,21 @@
 <style>
     .home{
         height : 100vh;
+        margin-top:10em;
     }
-
 </style>
+
 <div class = "container">
 <section class="py-5 text-center home">
     <div class="row py-lg-5">
+      <div class="col">
       <img src="assets/brand/licet-e1617087721530.png"></img>
+      </div>
       <div class="col-lg-6 col-md-8 mx-auto">
         <h1 class="fw-light">IEEE Licet</h1>
-        <p class="lead text-body-secondary">We at licet provide the best hands on experience to the students</p>
+        <p class="lead text-body-secondary">
+          We at licet provide the best hands on experience to the students
+        </p>
         <p>
           <a href="#" class="btn btn-primary my-2">Our goals</a>
         </p>
