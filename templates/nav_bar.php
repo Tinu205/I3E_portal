c5e028c28b61079956bc4c7deb95f147007c3716
Tinu205 <dazleroger404@gmail.com>
2024-01-15 19:53:21 +0530

    Added resposivenes for nav bar

--- templates/nav_bar.php
+++ templates/nav_bar.php
@@ -67,13 +67,16 @@
     padding-right:1vw;
   }
 </style>
+
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
   <div class="container-fluid">
-  <div class="navbar-collapse collapse d-flex justify-content-between" id="navbarCollapse">
-    <img src="assets/brand/ieee_blue.png" width="150" style="padding-left: 1vw;">
-    
-      <ul class="navbar-nav mb-2 mb-md-0">
-        <li class="nav-item">
+    <img src="assets/brand/ieee_resize 100.png">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+      <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#hom">Home</a>
         </li>
         <li class="nav-item">
@@ -92,4 +95,3 @@
     </div>
   </div>
 </nav>
-
